4e3058ef8aad26156a989bbe079c7461fb5aa5ee
Pavel <Zalyubovin.Pavel@vector-tech.io>
2025-06-11 18:29:04 +0300

    Add vee-validate module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,38 +1,44 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   pwa: {
-    registerType: "autoUpdate", // Автоматическое обновление service worker
+    registerType: 'autoUpdate', // Автоматическое обновление service worker
     manifest: {
-      name: "My Nuxt 3 PWA",
-      short_name: "NuxtPWA",
-      description: "My awesome Nuxt 3 Progressive Web App",
-      theme_color: "#ffffff",
-      background_color: "#ffffff",
-      display: "standalone",
-      scope: "/",
-      start_url: "/",
+      name: 'My Nuxt 3 PWA',
+      short_name: 'NuxtPWA',
+      description: 'My awesome Nuxt 3 Progressive Web App',
+      theme_color: '#ffffff',
+      background_color: '#ffffff',
+      display: 'standalone',
+      scope: '/',
+      start_url: '/',
       icons: [
         {
-          src: "/pwa-192x192.png",
-          sizes: "192x192",
-          type: "image/png",
+          src: '/pwa-192x192.png',
+          sizes: '192x192',
+          type: 'image/png',
         },
       ],
     },
     workbox: {
-      globPatterns: ["**/*.{js,css,html,ico,png,svg}"],
+      globPatterns: ['**/*.{js,css,html,ico,png,svg}'],
     },
-    includeAssets: ["favicon.ico", "robots.txt", "apple-touch-icon.png"],
+    includeAssets: ['favicon.ico', 'robots.txt', 'apple-touch-icon.png'],
     devOptions: {
       enabled: true,
-      type: "module",
+      type: 'module',
     },
     client: {
       installPrompt: true,
     },
   },
-  modules: ["@pinia/nuxt", "@vite-pwa/nuxt", "@vueuse/nuxt", "nuxt-swiper"],
-  css: ["~/assets/scss/main.scss"],
-  compatibilityDate: "2025-05-15",
+  modules: [
+    '@pinia/nuxt',
+    '@vite-pwa/nuxt',
+    '@vueuse/nuxt',
+    'nuxt-swiper',
+    '@vee-validate/nuxt',
+  ],
+  css: ['~/assets/scss/main.scss'],
+  compatibilityDate: '2025-05-15',
   devtools: { enabled: false },
 });
